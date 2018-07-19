3256c39e66724340727a8e5fa0b9021e240801b2
Elliott Alexander <escottalexander@gmail.com>
2018-07-19 18:26:31 -0400

    new callback example

--- async.js
+++ async.js
@@ -1,15 +1,24 @@
-const https = require('https');
+// const https = require('https');
 
 
-https.get('https://slayapps.com', (resp) => {
-    let data = '';
-    resp.on('data', (chunk) => {
-        data += chunk;
-    });
-    resp.on('end', () => {
-        console.log(data);
-    });
+// https.get('https://slayapps.com', (resp) => {
+//     let data = '';
+//     resp.on('data', (chunk) => {
+//         data += chunk;
+//     });
+//     resp.on('end', () => {
+//         console.log(data);
+//     });
 
-}).on('error', (err) => {
-    console.log("Error: " + err.message);
-});
+// }).on('error', (err) => {
+//     console.log("Error: " + err.message);
+// });
+
+function doHomework(subject, callback) {
+    alert(`Starting my ${subject} homework.`);
+    callback();
+}
+function alertFinished() {
+    alert('Finished my homework');
+}
+doHomework('math', alertFinished);
